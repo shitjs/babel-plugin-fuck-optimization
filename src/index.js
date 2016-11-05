27de60e2cf66e1cb99a71d4921e165e7d3471824
Igor Lobanov <bniwredyc@gmail.com>
2016-11-05 14:21:20 +0300

    Deoptimizations with "with" and __proto__ (#2)

--- src/index.js
+++ src/index.js
@@ -1,21 +1,46 @@
 module.exports = function (babel) {
   var t = babel.types;
 
+  var isDeoptimizablePath = function (path) {
+    return t.isFunctionDeclaration(path.parentPath) || t.isClassMethod(path.parentPath)
+  };
+
   return {
     visitor: {
       BlockStatement: function (path) {
-        if (
-          t.isFunctionDeclaration(path.parentPath) ||
-          t.isClassMethod(path.parentPath)
-        ) {
-          path.node.body.push(
-            t.tryStatement(
-              t.blockStatement([]),
-              t.catchClause(t.identifier('e'), t.blockStatement([])),
-              t.blockStatement([])
-            )
-          );
+        if (!isDeoptimizablePath(path)) {
+          return;
         }
+
+        var oldBody = path.node.body.slice(0);
+
+        path.node.body.length = 0;
+
+        var tryCatchStatement = t.tryStatement(
+          t.blockStatement([]),
+          t.catchClause(t.identifier('e'), t.blockStatement([])),
+          t.blockStatement([])
+        );
+
+        path.node.body.push(tryCatchStatement);
+
+        var deoptimizationVariableDeclaration = t.variableDeclaration('var', [
+          t.variableDeclarator(
+            t.identifier('deoptimization'),
+            t.objectExpression([
+              t.objectProperty(t.Identifier('__proto__'), t.stringLiteral('notsofast'))
+            ])
+          )
+        ]);
+
+        path.node.body.push(deoptimizationVariableDeclaration);
+
+        var withStatement = t.withStatement(
+          t.identifier('deoptimization'),
+          t.blockStatement(oldBody)
+        );
+
+        path.node.body.push(withStatement);
       }
     }
   }
